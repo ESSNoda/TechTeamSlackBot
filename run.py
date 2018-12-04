1bf5400663c165d219770c39b5e71fe3d198d317
EFEXP <ken1211mae@gmail.com>
2018-12-04 18:02:02 +0900

    中和

--- run.py
+++ run.py
@@ -1,9 +1,24 @@
+
 from slackbot.bot import Bot
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+import re
 
 
 def main():
     bot = Bot()
     bot.run()
+
+
+@respondo_to('中和')
+def what(message):
+    message.reply('花火が見えない？お前がいるから大丈夫。')
+    message.react('+1')
+
+
+@respond_to('(.*) ちょうだい')
+def giveme(message, something):
+    message.reply('はい、 {}だよ。'.format(something))
 
 
 if __name__ == "__main__":
