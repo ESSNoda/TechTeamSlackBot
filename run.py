5088a5d0c8118db7c1e16893ee23f5b9059931df
EFEXP <ken1211mae@gmail.com>
2018-12-04 18:32:34 +0900

    SWEET

--- run.py
+++ run.py
@@ -3,10 +3,13 @@ from slackbot.bot import Bot
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 from slackbot.bot import default_reply
+import random
 import re
 
 
 DEFAULT_REPLY = "Hello, I'm tech team bot!!"
+SWEET_SENTENCES = ["花火が見えない？お前がいるから大丈夫。", "恋はワインと同じ、時が経てばコクが出てくる",
+                   "お前の涙は俺だけの特権", "会うのに理由っている？", "帰りたいなんて言わせないよ"]
 
 
 def main():
@@ -16,13 +19,19 @@ def main():
 
 @respond_to('中和')
 def what(message):
-    message.send('花火が見えない？お前がいるから大丈夫。')
-    message.react('+1')
+    sentence = random.choice(SWEET_SENTENCES)
+    message.send(sentence)
+    message.react('yattaze')
 
 
 @respond_to('(.*)ちょうだい')
 def giveme(message, something):
     message.reply('はい、 {}だよ。'.format(something))
+
+
+@respond_to('(.*)くれ')
+def stmp(message, something):
+    message.reply('\stamp :{}:'.format(something))
 
 
 @default_reply
