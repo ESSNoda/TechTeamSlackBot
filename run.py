bd652a0a6d6046273028445084e07041f4087724
EFEXP <ken1211mae@gmail.com>
2018-12-08 23:08:27 +0900

    weather

--- run.py
+++ run.py
@@ -6,13 +6,25 @@ from slackbot.bot import default_reply
 import random
 import re
 import json
-import requests
+import aiohttp
+import asyncio
+import async_timeout
 
 DEFAULT_REPLY = "Hello, I'm Tech team bot!!"
 SWEET_SENTENCES = ["花火が見えない？お前がいるから大丈夫。", "恋はワインと同じ、時が経てばコクが出てくる",
                    "お前の涙は俺だけの特権", "会うのに理由っている？", "帰りたいなんて言わせないよ"]
-WEATHER = "http://api.openweathermap.org/data/2.5/weather?id={cityId}"
-APIKEY = "{あなたのAPIKEYを入れてください}"
+WEATHER = "http://api.openweathermap.org/data/2.5/weather?APPID=b71d7e713739dcd82a4fae311621eccc&id=1855078"
+
+
+async def getWeather(session):
+    async with aiohttp.ClientSession() as session:
+        with async_timeout.timeout(10):
+            async with session.get(WEATHER) as response:
+                html = await response.text()
+                data = json.loads(html)
+                message.send("野田の天気は…")
+                message.send(data["weather"][0]["main"])
+                message.send(":yarakashi:")
 
 
 def main():
@@ -32,28 +44,15 @@ def giveme(message, something):
     message.reply('はい、 {}だよ。'.format(something))
 
 
-@respond_to('(.*)くれ')
-def stmp(message, something):
-    message.reply('\stamp :{}:'.format(something))
-
-
 @respond_to('天気')
 async def weather(message):
-    result = await getWeather()
-    message.send(result["weather"][0]["description"])
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(getWeather())
 
 
 @default_reply
 def my_default_handler(message):
     message.reply(DEFAULT_REPLY)
-
-
-async def getWeather():
-    def k2c(k): return k - 273.15
-    url = WEATHER.format(cityId=1855078)
-    result = await requests.get(url)
-    data = json.loads(result.text)
-    return data
 
 
 if __name__ == "__main__":
